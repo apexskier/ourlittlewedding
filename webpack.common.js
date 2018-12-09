7271d7498ca27a4a77a90d0d96ea6ff3f6819a75
Cameron Little <cameron@camlittle.com>
2018-12-09 11:57:30 -0800

    Add thanks/sorry pages

--- webpack.common.js
+++ webpack.common.js
@@ -13,6 +13,8 @@ module.exports = {
     logistics: "./src/logistics",
     registry: "./src/registry",
     rsvp: "./src/rsvp",
+    "rsvp-thanks": "./src/rsvp-thanks",
+    "rsvp-sorry": "./src/rsvp-sorry",
     us: "./src/us",
   },
   output: {
